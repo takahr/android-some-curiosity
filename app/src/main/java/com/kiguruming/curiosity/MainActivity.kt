9bd9c706068e49108632b9831535a541ec98fff5
HIDAKA Takahiro <takahr@gmail.com>
2018-08-02 19:15:07 +0900

    use databinding for MainActivity.

--- app/src/main/java/com/kiguruming/curiosity/MainActivity.kt
+++ app/src/main/java/com/kiguruming/curiosity/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.kiguruming.curiosity
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kiguruming.curiosity.databinding.ActivityMainBinding
+import com.kiguruming.curiosity.loader.async.AsyncTaskLoaderActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.ui = UiListener()
+    }
+
+    inner class UiListener {
+        fun onClick() {
+        }
     }
 }
