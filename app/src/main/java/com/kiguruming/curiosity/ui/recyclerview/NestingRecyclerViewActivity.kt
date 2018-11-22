6450cfd394179ee9d5e52c8451ae9bb399d0ff7f
HIDAKA Takahiro <takahr@gmail.com>
2018-11-23 00:05:22 +0900

    Create Activity.

---
+++ app/src/main/java/com/kiguruming/curiosity/ui/recyclerview/NestingRecyclerViewActivity.kt
@@ -0,0 +1,13 @@
+package com.kiguruming.curiosity.ui.recyclerview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kiguruming.curiosity.R
+
+class NestingRecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nesting_recycler_view)
+    }
+}
