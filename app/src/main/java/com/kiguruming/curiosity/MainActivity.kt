ae46d3a2da60b19e787ec80776f2aef699a91fa0
HIDAKA Takahiro <takahr@gmail.com>
2018-11-22 23:23:49 +0900

    update project files to AndroidStudio 3.2.1.

--- app/src/main/java/com/kiguruming/curiosity/MainActivity.kt
+++ app/src/main/java/com/kiguruming/curiosity/MainActivity.kt
@@ -5,7 +5,6 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.kiguruming.curiosity.databinding.ActivityMainBinding
-import com.kiguruming.curiosity.loader.async.AsyncTaskLoaderActivity
 
 class MainActivity : AppCompatActivity() {
 
