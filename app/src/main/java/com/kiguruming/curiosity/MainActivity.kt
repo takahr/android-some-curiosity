7f663c4e18037df0e2831638352f2ed8a6e6b910
HIDAKA Takahiro <takahr@gmail.com>
2018-11-23 18:18:21 +0900

    migrate to androidx.

--- app/src/main/java/com/kiguruming/curiosity/MainActivity.kt
+++ app/src/main/java/com/kiguruming/curiosity/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.kiguruming.curiosity
 
 import android.content.Intent
-import android.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.kiguruming.curiosity.databinding.ActivityMainBinding
 import com.kiguruming.curiosity.ui.recyclerview.NestingRecyclerViewActivity
 
