31ec28bba28f56c900bb412a549ff8880e53ed96
HIDAKA Takahiro <takahr@gmail.com>
2018-11-23 19:05:57 +0900

    add FixedViewHolder.

--- app/src/main/java/com/kiguruming/curiosity/ui/recyclerview/NestingRecyclerViewActivity.kt
+++ app/src/main/java/com/kiguruming/curiosity/ui/recyclerview/NestingRecyclerViewActivity.kt
@@ -4,19 +4,26 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kiguruming.curiosity.R
+import com.kiguruming.curiosity.ui.recyclerview.adapter.OuterAdapter
+import kotlinx.android.synthetic.main.activity_nesting_recycler_view.*
 
 class NestingRecyclerViewActivity : AppCompatActivity() {
+    val adapter = OuterAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nesting_recycler_view)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
     companion object {
         fun createIntent(context: Context) : Intent {
-            val intent = Intent(context, NestingRecyclerViewActivity::class.java);
-            return intent;
+            val intent = Intent(context, NestingRecyclerViewActivity::class.java)
+            return intent
         }
     }
 }
--- app/src/main/java/com/kiguruming/curiosity/ui/recyclerview/adapter/OuterAdapter.kt
+++ app/src/main/java/com/kiguruming/curiosity/ui/recyclerview/adapter/OuterAdapter.kt
@@ -1,4 +1,51 @@
 package com.kiguruming.curiosity.ui.recyclerview.adapter
 
-class OuterAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kiguruming.curiosity.R
+
+class OuterAdapter : RecyclerView.Adapter<OuterAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        when (viewType) {
+            else -> {
+                return FixedViewHolder.create(inflater, parent)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return VIEWTYPE_FIXED
+    }
+
+    abstract class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    class FixedViewHolder(itemView: View) : ViewHolder(itemView) {
+
+        companion object {
+            fun create(inflater: LayoutInflater, parent: ViewGroup) : FixedViewHolder {
+                var view = inflater.inflate(R.layout.item_fixed, parent, false);
+                return FixedViewHolder(view)
+            }
+        }
+    }
+
+    companion object {
+        const val VIEWTYPE_FIXED = 1
+        const val VIEWTYPE_RECYCLER = 2
+    }
+
 }
